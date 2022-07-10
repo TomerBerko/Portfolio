5944b257cf07d25a8ae82bbdf036b913c43ca33b
TomerBerko <tomerberko2@gmail.com>
2022-07-10 18:38:31 +0300

    Some Change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/myAvatarAboutNoBack.png'
-import 'animate.css';
 
 
 const About = () => {
@@ -10,7 +9,7 @@ const About = () => {
       
       <h5>Get To Know</h5>
       <h2>About me</h2>
-      <div className='container about__container animate__animated animate__fadeInLeft'>
+      <div className='container about__container'>
         <div className='about__me'>
           <div className='about__me-image'>
             <img src={ME} alt="AboutImage" className='img_about_size' />
